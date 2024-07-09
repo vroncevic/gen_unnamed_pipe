74e5b99392995c1f30049744c2fd8e4440dbf7dc
Vladimir <elektron.ronca@gmail.com>
2024-07-09 21:39:40 +0200

    [gen_unnamed_pipe] Updated ATS CLI mechanism, docs

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ Info
 '''
 
 from __future__ import print_function
-from typing import List
+from typing import List, Optional
 from os.path import abspath, dirname, join
 from setuptools import setup
 
@@ -29,7 +29,7 @@ __author__ = 'Vladimir Roncevic'
 __copyright__ = '(C) 2017, https://vroncevic.github.io/gen_unnamed_pipe'
 __credits__: List[str] = ['Vladimir Roncevic', 'Python Software Foundation']
 __license__ = 'https://github.com/vroncevic/gen_unnamed_pipe/blob/dev/LICENSE'
-__version__ = '1.0.6'
+__version__ = '1.0.7'
 __maintainer__ = 'Vladimir Roncevic'
 __email__ = 'elektron.ronca@gmail.com'
 __status__ = 'Updated'
@@ -39,7 +39,7 @@ CONF: str = 'conf'
 TEMPLATE: str = 'conf/template'
 LOG: str = 'log'
 THIS_DIR: str = abspath(dirname(__file__))
-long_description: str | None = None
+long_description: Optional[str] = None
 with open(join(THIS_DIR, 'README.md'), encoding='utf-8') as readme:
     long_description = readme.read()
 PROGRAMMING_LANG: str = 'Programming Language :: Python ::'
@@ -61,7 +61,7 @@ APPROVED_LICENSES: List[str] = [
 PYP_CLASSIFIERS: List[str] = SUPPORTED_PY_VERSIONS + APPROVED_LICENSES
 setup(
     name='gen_unnamed_pipe',
-    version='1.0.6',
+    version='1.0.7',
     description='Generating Unnamed Pipe Modules',
     author='Vladimir Roncevic',
     author_email='elektron.ronca@gmail.com',
--- tests/run_coverage.sh
+++ tests/run_coverage.sh
@@ -7,7 +7,12 @@
 # @author  Vladimir Roncevic <elektron.ronca@gmail.com>
 #
 
+rm -rf htmlcov gen_unnamed_pipe_coverage.xml gen_unnamed_pipe_coverage.json .coverage
+rm -rf mytool
+ats_coverage_run.py -n gen_unnamed_pipe -p ../README.md
 rm -rf mytool
 python3 -m coverage run -m --source=../gen_unnamed_pipe unittest discover -s ./ -p '*_test.py' -vvv
-python3 -m coverage html
-
+python3 -m coverage html -d htmlcov
+python3 -m coverage xml -o gen_unnamed_pipe_coverage.xml 
+python3 -m coverage json -o gen_unnamed_pipe_coverage.json
+python3 -m coverage report --format=markdown -m
